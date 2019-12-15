7f425f2019cfc1c9338d2a75f7beabd5e70df759
Piotr J. Tomaszewski <pjtomaszewski98@gmail.com>
2019-12-15 21:00:49 +0100

    Add sprzet

--- InventoryManager/app.py
+++ InventoryManager/app.py
@@ -5,18 +5,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    strony = ['dodaj_oddzial', 'dodaj_magazyn', 'dodaj_sprzet']
+    return render_template('tmp/tymczasowy_index.html', strony=strony)
 
 
-@app.route('/dodaj_oddzial.html')
+@app.route('/dodaj_oddzial')
 def dodaj_oddzial():
     return render_template('add_modify/dodaj_oddzial.html')
 
 
-@app.route('/dodaj_magazyn.html')
+@app.route('/dodaj_magazyn')
 def dodaj_magazyn():
     oddzialy = ['Testowa 1', 'Kwiatowa 33', 'Krótka 5']
     return render_template('add_modify/dodaj_magazyn.html', oddzialy=oddzialy)
+
+
+@app.route('/dodaj_sprzet')
+def dodaj_sprzet():
+    domyslne = {'numer_ewidencyjny': 15, 'data_zakupu': '2019-12-16'}
+    magazyny = [{'numer': 1, 'oddzial': 'Testowa 1'}, {'numer': 2, 'oddzial': 'Testowa 1'},
+                {'numer': 3, 'oddzial': 'Kwiatowa 33'}, {'numer': 4, 'oddzial': 'Krótka 5'}]
+    return render_template('add_modify/dodaj_sprzet.html', magazyny=magazyny, domyslne=domyslne)
 
 
 if __name__ == '__main__':
